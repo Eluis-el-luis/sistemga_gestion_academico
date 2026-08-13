ab31d2b4437f591baee5b7fad541664d5f4c38ba
Eluis-el-luis <olivasalberto24@gmail.com>
2026-08-13 09:40:37 -0600

    Agregacion de funciones de relaciones en los Models

--- app/Models/Docente.php
+++ app/Models/Docente.php
@@ -8,4 +8,22 @@ class Docente extends Model
     use HasFactory;
     protected $table = 'docente';
     protected $fillable = ['usuario_id', 'codigo_unico_persona', 'sexo', 'es_coordinador', 'modalidad_coordina_id'];
+
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class, 'usuario_id');
+    }
+    // Relación: Modalidad que coordina (si aplica)
+    public function modalidadCoordina()
+    {
+        return $this->belongsTo(Modalidad::class, 'modalidad_coordina_id');
+    }
+
+    // Relación: Aulas donde es docente guía
+    public function aulasGuiadas()
+    {
+        return $this->hasMany(Aula::class, 'docente_guia_id');
+    }
+
 }
--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -8,4 +8,19 @@ class Matricula extends Model
     use HasFactory;
     protected $table = 'matricula';
     protected $fillable = ['alumno_id', 'aula_id', 'anio_escolar_id', 'estado', 'fecha_matricula', 'fecha_retiro'];
+
+    public function alumno()
+    {
+        return $this->belongsTo(Alumno::class, 'alumno_id');
+    }
+
+    public function aula()
+    {
+        return $this->belongsTo(Aula::class, 'aula_id');
+    }
+
+    public function anioEscolar()
+    {
+        return $this->belongsTo(AnioEscolar::class, 'anio_escolar_id');
+    }
 }
